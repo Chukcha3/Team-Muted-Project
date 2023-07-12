b1d6ac82dbae5a43d651259c228d4d48f52dc5b7
Chukcha3 <134764863+Chukcha3@users.noreply.github.com>
2023-07-12 18:29:07 +0300

    Open/Close inventory

--- My project (1)/Assets/Scripts/Inventory scripts/InventoryManager.cs
+++ My project (1)/Assets/Scripts/Inventory scripts/InventoryManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject inventory;
     public Transform SlotsPanel;
     public List<InventorySlot> slots = new List<InventorySlot>();
+    private bool isOpen = false;
     private void Start()
     {
+        inventory.SetActive(false);
         for (int i = 0; i < SlotsPanel.childCount; i++)
         {
             if (SlotsPanel.GetChild(i).GetComponent<InventorySlot>() != null)
             {
                 slots.Add(SlotsPanel.GetChild(i).GetComponent<InventorySlot>());
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isOpen = !isOpen;
+            if(isOpen)
+            {
+                inventory.SetActive(true);
+            }
+            else
+            {
+                inventory.SetActive(false);
             }
         }
     }
