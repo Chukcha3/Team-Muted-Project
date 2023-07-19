c83446d0bc60396b58850a537b2bd24d1b7199ab
Chukcha3 <134764863+Chukcha3@users.noreply.github.com>
2023-07-19 15:49:11 +0300

    Inventory UI

--- My project (1)/Assets/Scripts/Inventory scripts/InventorySlot.cs
+++ My project (1)/Assets/Scripts/Inventory scripts/InventorySlot.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
     public bool isEmpty = true;
     public ItemInfo item;
     public int itemAmount;
+    public TMP_Text itemAmountText;
+    public GameObject icon;
+
+    private void Start()
+    {
+        icon = transform.GetChild(0).gameObject;
+        itemAmountText = transform.GetChild(1).GetComponent<TMP_Text>();
+    }
+    public void SetIcon(Sprite icon)
+    {
+        this.icon.GetComponent<Image>().color = new Color(1,1,1,1);
+        this.icon.GetComponent<Image>().sprite = icon;
+    }
 }
