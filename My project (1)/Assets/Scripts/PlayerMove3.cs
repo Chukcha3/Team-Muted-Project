5d34d6bc26c7840f6b751cc2b0dde34842daa3ff
L-nine <am.vashchyshyn@gmail.com>
2024-01-20 09:43:06 +0200

    AnimationsUpdate

--- My project (1)/Assets/Scripts/PlayerMove3.cs
+++ My project (1)/Assets/Scripts/PlayerMove3.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMove3 : MonoBehaviour
 {
     [SerializeField] public GameObject muzzlePoint;
-    [SerializeField] float Speed = 1f, JumpForce = 5f;
+    [SerializeField] float Speed = 1f, JumpForce = 5f, HorizontalMove;
     [SerializeField] int MaxJumpsCount = 1;
+    [SerializeField] Animator animator;
+    bool Flip;
     public int Jump = 0;
     Rigidbody2D MyRigidbody2D;
     void Start()
@@ -16,11 +18,28 @@ public class PlayerMove3 : MonoBehaviour
     void Update()
     {
         MyRigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, MyRigidbody2D.velocity.y);
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * Speed;
+        if (HorizontalMove > 0 && Flip)
+        {
+            LookDirection();
+        }
+        else if (HorizontalMove < 0 && !Flip)
+        {
+            LookDirection();
+        }
+        animator.SetFloat("HorizontalMove", Mathf.Abs(HorizontalMove));   
         if (Input.GetKeyDown(KeyCode.Space) && MaxJumpsCount > Jump)
         {
             Jump++;
             //MyRigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, JumpForce);
-            MyRigidbody2D.AddForce(Vector2.up * JumpForce);
+            MyRigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
+    }
+    private void LookDirection()
+    {
+        Flip = !Flip;
+        Vector3 SC = transform.localScale;
+        SC.x *= -1;
+        transform.localScale = SC;
     }
 }
--- My project (1)/Assets/Scripts/isGrounded.cs
+++ My project (1)/Assets/Scripts/isGrounded.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class isGrounded : MonoBehaviour
 {
     [SerializeField]PlayerMove3 Script;
+    [SerializeField] float Check = 0;
+    [SerializeField] Animator animator;
     void Start()
     {
         
     }
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
+            Check += 1;
             Script.Jump = 0;
+            animator.SetBool("IsJump", false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (Check <= 1)
+            {
+                Script.Jump = 1;
+            }
+            Check -= 1;
+            animator.SetBool("IsJump", true);
         }
     }
 }
